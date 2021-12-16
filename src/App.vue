c70179f13e30ae0525ea494b72830e5579ef9580
fanison <yafanison@gmail.com>
2021-12-16 22:19:58 +0800

    修改html结构，配置router

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
 <template>
-  <div>导航栏 | 
-  <router-link to="/">HelloWorld</router-link> | 
-  <router-link to="/test">Test</router-link></div>
-  <hr/>
   <router-view />
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import {createWebHashHistory, createRouter} from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import Test from './components/Test.vue'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
 
 const history = createWebHashHistory()
 const router = createRouter({
     history: history,
     routes:[
-        {path: '/', component:HelloWorld},
-        {path:'/test',component:Test}
+        {path: '/', component:Home},
+        {path:'/doc',component:Doc}
     ]
 })
 
