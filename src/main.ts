47a6fa31bcab3d53f5fb8a8f69fb9ef582be2a97
yafanison <yafanison@gmail.com>
2022-01-02 14:31:36 +0800

    features 样式

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import './index.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
+import './lib/svg.js'
 
 const app = createApp(App)
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,15 +12,33 @@
       </div>
     </div>
     <div class="features">
-      <svg class="icon">
-        <use xlink:href="#icon-vue"></use>
-      </svg>
-      <svg class="icon">
-        <use xlink:href="#icon-ts"></use>
-      </svg>
-      <svg class="icon">
-        <use xlink:href="#icon-light"></use>
-      </svg>
+      <ul>
+        <li>
+          <svg class="icon">
+            <use xlink:href="#icon-vue"></use>
+          </svg>
+          <h3>基于 Vue 3</h3>
+          <p>骄傲地使用了 Vue 3 Compostion API</p>
+        </li>
+
+        <li>
+          <svg class="icon">
+            <use xlink:href="#icon-ts"></use>
+          </svg>
+          <h3>基于 TypeScript</h3>
+          <p>源代码采用 TypeScript 书写(非严格检查)</p>
+        </li>
+
+        <li>
+          <svg class="icon">
+            <use xlink:href="#icon-light"></use>
+          </svg>
+          <h3>代码易读</h3>
+          <p>每个组件的源代码都极其简洁</p>
+        </li>
+
+      </ul>
+
     </div>
   </div>
 
@@ -48,12 +66,51 @@ $color: #007974;
     rgba(227, 255, 253, 1) 0%,
     rgba(183, 233, 230, 1) 100%
   );
+  // 实现圆弧
+  clip-path: ellipse(80% 60% at 50% 40%);
 }
 
 .features {
-  > svg {
-    width: 64px;
-    height: 64px;
+  margin: 64ox auto;
+  width: 400px;
+
+  @media (min-width: 800px) {
+    width: 800px;
+  }
+
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+
+  > ul {
+    display: flex;
+    flex-wrap: wrap;
+
+    > li {
+      width: 400px;
+      margin: 16px 0;
+      display: grid;
+      justify-content: start;
+      align-content: space-between;
+      grid-template-areas: "icon title" "icon text";
+      grid-template-rows: 1fr auto;
+      grid-template-columns: 80px auto;
+
+      > svg {
+        grid-area: icon;
+        width: 64px;
+        height: 64px;
+      }
+
+      > h3 {
+        grid-area: title;
+        font-size: 28px;
+      }
+
+      > p {
+        grid-area: text;
+      }
+    }
   }
 }
 
