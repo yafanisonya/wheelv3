9112177fd6ee8ee50dec2e4fd5be7b83d1b6bbdc
yafanison <yafanison@gmail.com>
2022-01-04 20:53:34 +0800

    改造 Dialog demo

---
+++ src/components/Dialog1.demo.vue
@@ -0,0 +1,53 @@
+<demo>常规使用</demo>
+<template>
+  <div>
+    <Button @click="toggle">toggle</Button>
+    <Dialog
+      v-model:visible="x"
+      :closeOnClickOverlay="false"
+      :ok="f1"
+      :cancel="f2"
+    >
+      <template v-slot:title>
+        <strong>加粗的标题</strong>
+      </template>
+
+      <template v-slot:content>
+        <strong>hi</strong>
+        <div>hi 2</div>
+      </template>
+    </Dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import Dialog from "../lib/Dialog.vue";
+import Button from "../lib/Button.vue";
+import { ref } from "vue";
+
+export default {
+  components: {
+    Dialog,
+    Button,
+  },
+  setup() {
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+
+    const f1 = () => {
+      return false;
+    };
+
+    const f2 = () => {};
+
+    return {
+      x,
+      toggle,
+      f1,
+      f2,
+    };
+  },
+};
+</script>
