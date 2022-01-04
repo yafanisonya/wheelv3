e73b932bc68a0357a00b079bc9ce3bdf5509e584
yafanison <yafanison@gmail.com>
2022-01-04 19:49:05 +0800

    使用 prism 高亮代码显示

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -10,7 +10,10 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>{{Switch1Demo.__sourceCode}}</pre>
+        <pre
+          class="language-html"
+          v-html="Prism.highlight(Switch1Demo.__sourceCode,Prism.languages.html,'html')"
+        />
       </div>
     </div>
 
@@ -23,7 +26,10 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>{{Switch2Demo.__sourceCode}}</pre>
+        <pre
+          class="language-html"
+          v-html="Prism.highlight(Switch2Demo.__sourceCode,Prism.languages.html,'html')"
+        />
       </div>
     </div>
   </div>
@@ -31,10 +37,12 @@
 
 <script lang="ts">
 import { ref } from "vue";
-import Switch from "../lib/Switch.vue";
 import Button from "../lib/Button.vue";
 import Switch1Demo from "./Switch1.demo.vue";
 import Switch2Demo from "./Switch2.demo.vue";
+import "prismjs";
+import "prismjs/themes/prism.css";
+const Prism = (window as any).Prism;
 
 export default {
   components: {
@@ -42,7 +50,7 @@ export default {
   },
   setup() {
     const bool = ref(false);
-    return { bool, Switch1Demo, Switch2Demo };
+    return { bool, Switch1Demo, Switch2Demo, Prism };
   },
 };
 </script>
