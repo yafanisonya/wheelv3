641542458a77af992c6cbec8779fcbf313df24fb
yafanison <yafanison@gmail.com>
2022-01-06 20:10:53 +0800

    调整相关介绍，修改配置 ignore

--- src/components/MarkDown.vue
+++ src/components/MarkDown.vue
@@ -2,26 +2,17 @@
   <article
     class="markdown-body"
     v-html="content"
-  ></article>
+  >
+  </article>
 </template>
 
 <script lang="ts">
-import { ref } from "vue";
 export default {
   props: {
-    path: {
+    content: {
       type: String,
       required: true,
     },
-  },
-  setup(props) {
-    const content = ref<string>(null);
-    import(props.path).then((result) => {
-      content.value = result.default;
-    });
-    return {
-      content,
-    };
   },
 };
 </script>
