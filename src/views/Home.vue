01a07bac95e556e3b42aebed76783129b831f3d9
yafanison <yafanison@gmail.com>
2022-01-14 09:49:28 +0800

    update github url

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
         <h1>Fanison UI</h1>
         <h2>一个厉害的 UI 框架</h2>
         <p class="actions">
-          <a href="https://github.com">Blog</a>
+          <a href="https://github.com/yafanisonya">Github</a>
           <router-link to="/doc">开始</router-link>
         </p>
       </div>
